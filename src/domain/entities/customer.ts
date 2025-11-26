3e65681bd871cf2e1fa8a1e694beb23be0bbae6a
Digos <diogobissolimoreno@gmail.com>
2025-11-26 16:20:57 -0300

    feat: entidade cliente

---
+++ src/domain/entities/customer.ts
@@ -0,0 +1,15 @@
+import CNPJ from "../vo/cnpj";
+import CPF from "../vo/cpf";
+import Entity from "./entity";
+
+export default interface Customer extends Entity {
+  name: string
+  Cnpj?: CNPJ
+  Cpf?: CPF
+  CustomerContact?: CustomerContact
+}
+
+export interface CustomerContact {
+
+}
+
