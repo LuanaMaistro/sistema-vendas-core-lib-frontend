1d716c0afd00591fa5da2ef743e3e981e183a4ec
LuanaMaistro <luanamaistro@gmail.com>
2025-11-26 16:21:52 -0300

    feat: interface para CRUD generico

---
+++ src/application/interfaces/services/crudService.ts
@@ -0,0 +1,8 @@
+import Result from "./result";
+
+export default interface CrudService<T> {
+  Add(entity: T): Result
+  Remove(entity: T): Result
+  Update(entity: T): Result
+  List(): Result<Array<T>>
+}
