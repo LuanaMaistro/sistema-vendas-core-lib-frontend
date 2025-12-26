92d1c393cc1cfa1eee4942a71cc92c19d1aab2ac
LuanaMaistro <luanamaistro@gmail.com>
2025-12-26 20:01:08 -0300

    feat: método para adicionar cliente cnpj

--- src/application/useCases/customer/addCustomer/addCustomer.ts
+++ src/application/useCases/customer/addCustomer/addCustomer.ts
@@ -1,9 +1,11 @@
 import Customer from "../../../../domain/entities/customer"
+import CNPJ from "../../../../domain/vo/cnpj"
 import CPF from "../../../../domain/vo/cpf"
 import { ListServices, PickServices } from "../../../interfaces/services"
 import CustomerService from "../../../interfaces/services/customerService"
 import UseCase from "../../base/abstractUseCase"
 import { OperationResult } from "../../base/OperationResult"
+import AddCnpjCustomerDto from "./addCnpjCustomerDto"
 import AddCpfCustomerDto from "./addCpfCustomerDto"
 
 export default class AddCustomer extends UseCase{
@@ -32,5 +34,18 @@ export default class AddCustomer extends UseCase{
     return this.toOperationResult(this.customerServices.Add(newCustomer))
   }
 
+  async addCnpj(aCpfCustomer: AddCnpjCustomerDto): Promise<OperationResult> {
+
+    const newCustomer = {
+      Cnpj: CNPJ.create(aCpfCustomer.cnpj),
+      name: aCpfCustomer.name,
+      CustomerContact: {
+        email: aCpfCustomer.email,
+        phone: aCpfCustomer.phone,
+      }
+    } as Customer
+
+    return this.toOperationResult(this.customerServices.Add(newCustomer))
+  }
 }
 
