37ef89d6d22c904aa7251b6e0874fdb9695d7dca
Digos <diogobissolimoreno@gmail.com>
2025-12-26 19:59:29 -0300

    feat: dtos para adição de clientes

---
+++ src/application/useCases/customer/addCustomer/addCnpjCustomerDto.ts
@@ -0,0 +1,6 @@
+export default interface AddCnpjCustomerDto {
+  name: string,
+  cnpj: string,
+  email: string,
+  phone: string,
+}
---
+++ src/application/useCases/customer/addCustomer/addCpfCustomerDto.ts
@@ -0,0 +1,6 @@
+export default interface AddCpfCustomerDto {
+  name: string,
+  cpf: string,
+  email: string,
+  phone: string,
+}
