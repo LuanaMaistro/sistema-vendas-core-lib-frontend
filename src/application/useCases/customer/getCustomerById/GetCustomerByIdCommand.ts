315bdf839d1b8e9a4f42565e5cde06d9b281bd4c
Digos <diogobissolimoreno@gmail.com>
2025-12-27 00:39:23 -0300

    feat: get customer by id use case

---
+++ src/application/useCases/customer/getCustomerById/GetCustomerByIdCommand.ts
@@ -0,0 +1,3 @@
+export default interface GetCustomerByIdCommand {
+  id: string
+}
---
+++ src/application/useCases/customer/getCustomerById/getCustomerById.ts
@@ -0,0 +1,25 @@
+import Customer from "../../../../domain/entities/customer";
+import { left } from "../../../either/either";
+import { ListServices, PickServices } from "../../../interfaces/services";
+import CustomerService from "../../../interfaces/services/customerService";
+import UseCase from "../../base/abstractUseCase";
+import { OperationResult } from "../../base/OperationResult";
+import GetCustomerByIdCommand from "./GetCustomerByIdCommand";
+
+export default class GetCustomerByIdUseCase extends UseCase {
+
+  static inject: Array<ListServices> = ['CustomerSerivce'] as const
+
+  private readonly customerServices: CustomerService
+
+  constructor(services: PickServices<'CustomerSerivce'>) {
+    super()
+    this.customerServices = services.CustomerSerivce
+  }
+
+  async execute(command: GetCustomerByIdCommand): Promise<OperationResult<Customer>> {
+    if(!command.id) return left(new Error("ID é obrigatório"))
+
+    return this.toOperationResult<Customer>(this.customerServices.GetById(command.id))
+  }
+}
