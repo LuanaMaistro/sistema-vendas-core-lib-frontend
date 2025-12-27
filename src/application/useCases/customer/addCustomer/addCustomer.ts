bb4f2ec18be1af113c14dfd90863e77311c2c1af
Digos <diogobissolimoreno@gmail.com>
2025-12-26 22:12:13 -0300

    refactor: removendo métodos duplicados para adicionar clientes

--- src/application/useCases/customer/addCustomer/addCustomer.ts
+++ src/application/useCases/customer/addCustomer/addCustomer.ts
@@ -1,13 +1,12 @@
 import Customer from "../../../../domain/entities/customer"
 import CNPJ from "../../../../domain/vo/cnpj"
 import CPF from "../../../../domain/vo/cpf"
-import { left, right } from "../../../either"
+import { left } from "../../../either"
 import { ListServices, PickServices } from "../../../interfaces/services"
 import CustomerService from "../../../interfaces/services/customerService"
 import UseCase from "../../base/abstractUseCase"
 import { OperationResult } from "../../base/OperationResult"
-import AddCnpjCustomerDto from "./addCnpjCustomerDto"
-import AddCpfCustomerDto from "./addCpfCustomerDto"
+import AddCustomerCommand from "./AddCustomerDto"
 
 export default class AddCustomerUseCase extends UseCase{
 
@@ -21,36 +20,28 @@ export default class AddCustomerUseCase extends UseCase{
 
   }
 
-  async execute(): Promise<OperationResult> {
-    return right(undefined)
+  async execute(command: AddCustomerCommand): Promise<OperationResult> {
+    if(!command.cpf && !command.cnpj) left(new Error("É necessário pelo menos um documento"))
+
+    const customer = this.createCustomer(command)
+    return this.toOperationResult(this.customerServices.Add(customer))
   }
 
-  async addCpf(aCpfCustomer: AddCpfCustomerDto): Promise<OperationResult> {
-
-    const newCustomer = {
-      Cpf: CPF.create(aCpfCustomer.cpf),
-      name: aCpfCustomer.name,
+  private createCustomer(command: AddCustomerCommand): Customer {
+    const result = {
+      name: command.name,
       CustomerContact: {
-        email: aCpfCustomer.email,
-        phone: aCpfCustomer.phone,
+        email: command.email,
+        phone: command.phone,
       }
     } as Customer
 
-    return this.toOperationResult(this.customerServices.Add(newCustomer))
+
+    if(command.cpf) result.Cpf = CPF.create(command.cpf)
+      else result.Cnpj = CNPJ.create(command.cnpj!)
+
+    return result
   }
 
-  async addCnpj(aCpfCustomer: AddCnpjCustomerDto): Promise<OperationResult> {
-
-    const newCustomer = {
-      Cnpj: CNPJ.create(aCpfCustomer.cnpj),
-      name: aCpfCustomer.name,
-      CustomerContact: {
-        email: aCpfCustomer.email,
-        phone: aCpfCustomer.phone,
-      }
-    } as Customer
-
-    return this.toOperationResult(this.customerServices.Add(newCustomer))
-  }
 }
 
