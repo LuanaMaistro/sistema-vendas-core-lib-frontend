8d47bc86849403371b205e336ae834ef829d9c60
Digos <diogobissolimoreno@gmail.com>
2025-12-27 00:39:12 -0300

    feat: list customer use case

---
+++ src/application/useCases/customer/listCustomers/listCustomers.ts
@@ -0,0 +1,21 @@
+import Customer from "../../../../domain/entities/customer";
+import { ListServices, PickServices } from "../../../interfaces/services";
+import CustomerService from "../../../interfaces/services/customerService";
+import UseCase from "../../base/abstractUseCase";
+import { OperationResult } from "../../base/OperationResult";
+
+export default class ListCustomersUseCase extends UseCase {
+
+  static inject: Array<ListServices> = ['CustomerSerivce'] as const
+
+  private readonly customerServices: CustomerService
+
+  constructor(services: PickServices<'CustomerSerivce'>) {
+    super()
+    this.customerServices = services.CustomerSerivce
+  }
+
+  async execute(): Promise<OperationResult<Array<Customer>>> {
+    return this.toOperationResult(this.customerServices.List())
+  }
+}
