6c1b5d1ddc76c16c40617126f8eb0c9b8fc33be3
LuanaMaistro <luanamaistro@gmail.com>
2025-12-29 10:41:20 -0300

    feat: index para useCases/base

---
+++ src/application/useCases/base/index.ts
@@ -0,0 +1,6 @@
+export { default as AbstractUseCase } from './abstractUseCase'
+export type { OperationResult } from './OperationResult'
+
+export { default as OperationError } from './errors/OperationError'
+export { default as ClientError } from './errors/ClientError'
+export { default as ServerError } from './errors/ServerError'
