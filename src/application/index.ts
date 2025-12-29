905aca756c1abf2578a03943406342529a7ece42
Digos <diogobissolimoreno@gmail.com>
2025-12-29 10:42:21 -0300

    feat: exportando modulos extras da aplicação

--- src/application/index.ts
+++ src/application/index.ts
@@ -1,2 +1,6 @@
-export * as UsesCases from './useCases'
+export * as UseCases from './useCases'
 export * as Services from './interfaces/services'
+export * as UseCasesBase from './useCases/base'
+export type { UseCaseInstances } from './types'
+export * from './either'
+
