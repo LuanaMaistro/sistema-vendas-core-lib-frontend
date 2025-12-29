85a2eae50fd47a7da154275c8a7852658adb7442
LuanaMaistro <luanamaistro@gmail.com>
2025-12-29 10:42:46 -0300

    fix: UsesCases para UseCase

--- src/application/types.ts
+++ src/application/types.ts
@@ -1,7 +1,6 @@
 import * as Application from "./index";
-
 export type UseCaseInstances = {
-  [K in keyof typeof Application.UsesCases as Capitalize<K & string>]:
-    InstanceType<typeof Application.UsesCases[K]>
+  [K in keyof typeof Application.UseCases as Capitalize<K & string>]:
+    InstanceType<typeof Application.UseCases[K]>
 }
 
