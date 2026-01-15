641eec4f4cfe6a705510b7e3ad91744aedc652a7
Digos <diogobissolimoreno@gmail.com>
2026-01-15 10:38:41 -0300

    feat: value object para preço

---
+++ src/domain/vo/price.ts
@@ -0,0 +1,26 @@
+import ValueObjectError from "../errors/valueObjectError"
+
+export default class Price {
+  private constructor(private value: number) {}
+
+  get Value() {
+    return this.value;
+  }
+
+  static create(price: number | string): Price {
+    const numericPrice = typeof price === 'string' ? parseFloat(price) : price;
+
+    if (!this.isValid(numericPrice)) {
+      throw new ValueObjectError("Preço inválido");
+    }
+
+    return new Price(numericPrice);
+  }
+
+  private static isValid(price: number): boolean {
+    if (isNaN(price)) return false;
+    if (price < 0) return false;
+    if (!isFinite(price)) return false;
+    return true;
+  }
+}
