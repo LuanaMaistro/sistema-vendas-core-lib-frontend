31d4cfd42926a709d55b371eed693021e1352c1f
LuanaMaistro <luanamaistro@gmail.com>
2026-01-15 10:40:07 -0300

    feat: adicionando campos de preço e quantidade para produto

--- src/domain/entities/product.ts
+++ src/domain/entities/product.ts
@@ -1,7 +1,12 @@
 import Entity from "./entity";
+import Price from "../vo/price";
+import Quantity from "../vo/quantity";
+
 export default interface Product extends Entity {
   name: string;
   description: string;
   code: string;
+  price: Price;
+  quantity: Quantity;
 }
 
