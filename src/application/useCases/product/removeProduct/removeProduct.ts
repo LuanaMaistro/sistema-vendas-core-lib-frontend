52cc8a611e5730019538c443772df36289d5b711
Digos <diogobissolimoreno@gmail.com>
2026-01-15 11:06:24 -0300

    feat: use case para remover produto

---
+++ src/application/useCases/product/removeProduct/removeProduct.ts
@@ -0,0 +1,23 @@
+import { left } from "../../../either/either";
+import { ListServices, PickServices } from "../../../interfaces/services";
+import ProductService from "../../../interfaces/services/productService";
+import UseCase from "../../base/abstractUseCase";
+import { OperationResult } from "../../base/OperationResult";
+import RemoveProductCommand from "./RemoveProductCommand";
+
+export default class RemoveProductUseCase extends UseCase {
+  static inject: Array<ListServices> = ['ProductService'] as const
+
+  private readonly productService: ProductService
+
+  constructor(services: PickServices<'ProductService'>) {
+    super()
+    this.productService = services.ProductService
+  }
+
+  async execute(command: RemoveProductCommand): Promise<OperationResult> {
+    if (!command.id) return left(new Error("ID é obrigatório"));
+
+    return this.toOperationResult(this.productService.Remove(command.id));
+  }
+}
