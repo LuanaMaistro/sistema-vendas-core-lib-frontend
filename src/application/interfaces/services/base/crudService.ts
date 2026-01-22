0377aeece245f691ac1ffcba570d652efbcd18c6
Digos <diogobissolimoreno@gmail.com>
2026-01-22 16:46:19 -0300

    fix: função de list async

--- src/application/interfaces/services/base/crudService.ts
+++ src/application/interfaces/services/base/crudService.ts
@@ -4,7 +4,7 @@ export default interface CrudService<T> {
   Add(entity: T): Result
   Remove(id: string): Result
   Update(entity: T): Result
-  List(): Result<Array<T>>
+  List(): Promise<Result<Array<T>>>
   GetById(id: string): Result<T>
 }
 
