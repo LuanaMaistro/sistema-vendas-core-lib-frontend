54a348172e1af0761f54bd71906f3d3a3430e69e
LuanaMaistro <luanamaistro@gmail.com>
2026-01-28 14:17:29 -0300

    feat: método para filtrar clientes com filtros

--- src/application/interfaces/services/customerService.ts
+++ src/application/interfaces/services/customerService.ts
@@ -1,7 +1,13 @@
 import {Customer} from "../../../domain/entities/customer"
+import { Result } from "./base";
 import CrudService from "./base/crudService";
 
 
 export default interface CustomerService extends CrudService<Customer> {
-
+  ListCustomers(filters: ListCustomerFilters): Promise<Result<Array<Customer>>>
 }
+
+export interface ListCustomerFilters {
+  onlyInactive?: boolean
+}
+
--- src/application/useCases/customer/listCustomers/listCustomers.ts
+++ src/application/useCases/customer/listCustomers/listCustomers.ts
@@ -3,6 +3,7 @@ import { ListServices, PickServices } from "../../../interfaces/services";
 import CustomerService from "../../../interfaces/services/customerService";
 import UseCase from "../../base/abstractUseCase";
 import { OperationResult } from "../../base/OperationResult";
+import ListCustomersCommand from "./ListCustomersCommand";
 
 export default class ListCustomersUseCase extends UseCase {
 
@@ -15,8 +16,14 @@ export default class ListCustomersUseCase extends UseCase {
     this.customerServices = services.CustomerService
   }
 
-  async execute(): Promise<OperationResult<Array<Customer>>> {
-    const serviceResult = await this.customerServices.List()
+  async execute(filters?: ListCustomersCommand): Promise<OperationResult<Array<Customer>>> {
+    if(!filters)
+      return await this.toOperationResult(await this.customerServices.List())
+
+    const serviceResult = await this.customerServices.ListCustomers({
+      onlyInactive: filters.onlyInactive
+    })
     return await this.toOperationResult(serviceResult)
   }
 }
+
