7a9cc9bfb6f2dc2b2ab3b187fdc66b296a16fa76
Digos <diogobissolimoreno@gmail.com>
2026-01-28 16:07:54 -0300

    test: teste

--- src/main.ts
+++ src/main.ts
@@ -54,6 +54,7 @@ export default function createApplicationInstance(factoriesServices: ServicesFac
     }, {})
 
     const instance = new (aUseCaseClass as any)(resolvedServices)
+    console.log('teste', instance)
     result[name] = createProxyUseCase(instance)
   }
 
