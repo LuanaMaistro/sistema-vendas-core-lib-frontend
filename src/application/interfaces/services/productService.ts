210d8d2068b5a9b95af117483ce19b4f204aa9b4
LuanaMaistro <luanamaistro@gmail.com>
2026-03-22 16:28:24 -0300

    feat: métodos para ativar/desativar produtos

--- src/application/interfaces/services/productService.ts
+++ src/application/interfaces/services/productService.ts
@@ -1,6 +1,8 @@
 import Product from "../../../domain/entities/product";
 import CrudService from "./base/crudService";
+import Result from "./base/result";
 
 export default interface ProductService extends CrudService<Product> {
-
+  Activate(id: string): Promise<Result>
+  Deactivate(id: string): Promise<Result>
 }
