d6b765fef220a37207ffc7f258798fa264fee303
Digos <diogobissolimoreno@gmail.com>
2026-03-23 10:51:22 -0300

    refactor: id -> productId

--- src/application/interfaces/services/productService.ts
+++ src/application/interfaces/services/productService.ts
@@ -4,8 +4,8 @@ import CrudService from "./base/crudService";
 import Result from "./base/result";
 
 export default interface ProductService extends CrudService<Product> {
-  Activate(id: string): Promise<Result>
-  Deactivate(id: string): Promise<Result>
+  Activate(productId: string): Promise<Result>
+  Deactivate(productId: string): Promise<Result>
   AddStock(productId: string, quantity: Quantity): Promise<Result<Product>>
   RemoveStock(productId: string, quantity: Quantity): Promise<Result<Product>>
 }
