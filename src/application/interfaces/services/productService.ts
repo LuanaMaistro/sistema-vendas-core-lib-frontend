05931061c3ca75d6a1a89593517c7013e6964735
Digos <diogobissolimoreno@gmail.com>
2026-03-23 10:51:05 -0300

    feat: métodos para movimentação de stock

--- src/application/interfaces/services/productService.ts
+++ src/application/interfaces/services/productService.ts
@@ -1,8 +1,11 @@
 import Product from "../../../domain/entities/product";
+import Quantity from "../../../domain/vo/quantity";
 import CrudService from "./base/crudService";
 import Result from "./base/result";
 
 export default interface ProductService extends CrudService<Product> {
   Activate(id: string): Promise<Result>
   Deactivate(id: string): Promise<Result>
+  AddStock(productId: string, quantity: Quantity): Promise<Result<Product>>
+  RemoveStock(productId: string, quantity: Quantity): Promise<Result<Product>>
 }
